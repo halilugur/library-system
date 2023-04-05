597b5185f8f1ba0c5277075030c6cd8615578b6e
Tolga Baris Pinar <68820772+tolgabp@users.noreply.github.com>
2023-04-05 13:06:14 +0100

    writeCSV method was created in CSVUtil class

--- src/system/utils/CSVUtil.java
+++ src/system/utils/CSVUtil.java
@@ -1,17 +1,18 @@
 package system.utils;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  * This class read and write to CSV file.
@@ -64,17 +65,30 @@ public class CSVUtil {
     public static void writeCSV(String path, String messageStart, String messageEnd) {
         System.out.println(messageStart);
 
-//        // first create file object for file placed at location
-//        // specified by file's path
-//        File file = new File(path);
-//
-//        try {
-//            FileWriter fileWriter = new FileWriter(file);
-//            CSVWriter csvWriter = new CSVWriter(fileWriter);
-//
-//            csvWriter;
-//        } catch (IOException e) {
-//        }
+        try {
+            // Create a FileWriter and BufferedWriter to write to the CSV file
+            FileWriter fileWriter = new FileWriter(path, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Prompt the user to enter new data to add to the CSV file
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter new data to add to the CSV file: ");
+            String newData = scanner.nextLine();
+
+            //write the new data to the CSV file
+            bufferedWriter.write(newData);
+            bufferedWriter.newLine();
+
+            //Close the file
+            bufferedWriter.close();
+            fileWriter.close();
+
+            System.out.println("Successfully wrote to the CSV file.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while wirting to CSV file:");
+            e.printStackTrace();
+        }
 
         System.out.println(messageEnd);
     }
